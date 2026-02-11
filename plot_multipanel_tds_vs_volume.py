3a0bd30d9d1f373d16aee17a1848dbed04e749a2
RadostW <radost.waszkiewicz@gmail.com>
2026-02-11 09:53:34 +0100

    Disolved solids as a function of volume

---
+++ plot_multipanel_tds_vs_volume.py
@@ -0,0 +1,184 @@
+import pathlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# =========================
+# Paths
+# =========================
+HERE = pathlib.Path(__file__).resolve().parent
+TDS_DATA = HERE / "formatted_measurements" / "tds.csv"
+TIME_DEPENDENT_DATA = HERE / "formatted_measurements" / "time_dependent.csv"
+
+TDS_FIT_CSV = HERE / "fit_parameters" / "tds_calibration.csv"
+SOLIDS_FIT_CSV = HERE / "fit_parameters" / "solids_calibration.csv"
+
+STYLE_FILE = HERE / "styles" / "espresso.mplstyle"
+
+OUTPUT_DIR = HERE / "figures"
+OUTPUT_DIR.mkdir(exist_ok=True)
+OUTPUT_FILE = OUTPUT_DIR / "multipanel_brewer_calibration.pdf"
+
+# =========================
+# Figure settings
+# =========================
+FIG_WIDTH = 7
+FIG_HEIGHT = 2
+FONT_SIZE = 9
+
+plt.style.use(STYLE_FILE)
+plt.rcParams.update({"lines.marker": ""})  # no default markers
+SHADOW_ALPHA = 0.25
+
+
+# =========================
+# Helper function for consistent error bars
+# =========================
+def ax_errorbar_black(ax, *args, **kwargs):
+    """
+    Wrapper around ax.errorbar that defaults ecolor to black,
+    sets thin vertical lines, caps, and no connecting lines.
+    """
+    kwargs.setdefault("ecolor", "black")  # error bars color
+    kwargs.setdefault("elinewidth", 0.8)  # vertical line width
+    kwargs.setdefault("ls", "")  # no connecting line
+    return ax.errorbar(*args, **kwargs)
+
+
+# =========================
+# Load data
+# =========================
+
+fit_tds_params = pd.read_csv(TDS_FIT_CSV).set_index("parameter")["value"]
+k_tds = fit_tds_params["k__percent"]
+l_tds = fit_tds_params["l__s"]
+m_tds = fit_tds_params["m__s"]
+
+
+fit_solids_params = pd.read_csv(SOLIDS_FIT_CSV).set_index("parameter")["value"]
+k_solids = fit_solids_params["k_solids__g"]
+l_solids = fit_solids_params["l_solids__s"]
+m_solids = fit_solids_params["m_solids__s"]
+first_drop_offset = fit_solids_params["first_drop_offset__s"]
+
+df_tds = pd.read_csv(TDS_DATA)
+df_time_dependent_all = pd.read_csv(TIME_DEPENDENT_DATA)
+df_time_dependent = df_time_dependent_all[
+    df_time_dependent_all["reference_pressure_round__bar"] == 9.0
+].reset_index(drop=True)
+
+
+# =========================
+# Models
+# =========================
+def tds_teo(t, k_tds, l_tds, m_tds):
+    return 0.5 * k_tds * (1 - np.tanh((t - l_tds) / m_tds))
+
+
+def solids_teo(t, k_solids, l_solids, m_solids):
+    return 0.5 * k_solids * (1 + np.tanh((t - l_solids) / m_solids))
+
+
+# =========================
+# Create figure
+# =========================
+fig, axs = plt.subplots(
+    1,
+    2,
+    figsize=(FIG_WIDTH, FIG_HEIGHT),
+    width_ratios=(1, 1),
+    constrained_layout=True,
+    # sharey=True,
+)
+
+ax_volume, ax_solutes = axs
+
+# =
+# Plot
+# =
+
+# tds_teo_time = np.linspace(df_tds["time__s"].min(), df_tds["time__s"].max(), 400)
+tds_teo_time = np.linspace(-first_drop_offset, df_tds["time__s"].max(), 400)
+tds_teo_value = tds_teo(tds_teo_time, k_tds, l_tds, m_tds)
+
+solids_teo_time = np.linspace(
+    df_time_dependent["time__s"].min(), df_time_dependent["time__s"].max(), 400
+)
+solids_teo_value = solids_teo(
+    solids_teo_time - first_drop_offset, k_solids, l_solids, m_solids
+)
+
+df_time_dependent["tds__percent"] = tds_teo(
+    df_time_dependent["time__s"] - first_drop_offset,
+    k_tds,
+    l_tds,
+    m_tds,
+)
+df_time_dependent["solids_flow__g_per_s"] = (
+    df_time_dependent["tds__percent"]
+    / 100.0
+    * df_time_dependent["mass_flow_rate__g_per_s"]
+)
+df_time_dependent["solids_flow_std__g_per_s"] = (
+    df_time_dependent["tds__percent"]
+    / 100.0
+    * df_time_dependent["mass_flow_rate_std__g_per_s"]
+)
+dt = df_time_dependent["time__s"].iloc[1] - df_time_dependent["time__s"].iloc[0]
+df_time_dependent["solids_removed__g"] = (
+    df_time_dependent["solids_flow__g_per_s"].cumsum() * dt
+)
+
+time = df_time_dependent["time__s"]
+mean_flow = df_time_dependent["mass_flow_rate__g_per_s"]
+sigma_flow = df_time_dependent["mass_flow_rate_std__g_per_s"]
+
+mean_flow_solids = df_time_dependent["solids_flow__g_per_s"]
+sigma_flow_solids = df_time_dependent["solids_flow_std__g_per_s"]
+
+df_time_dependent["mass__g"] = df_time_dependent["mass_flow_rate__g_per_s"].cumsum() * dt
+df_time_dependent["solids__g"] = df_time_dependent["solids_flow__g_per_s"].cumsum() * dt
+
+ax_volume.plot(
+    df_time_dependent["mass__g"],
+    df_time_dependent["mass_flow_rate__g_per_s"]
+)
+
+ax_solutes.plot(
+    df_time_dependent["solids__g"],
+    df_time_dependent["mass_flow_rate__g_per_s"]
+)
+
+# =========================
+# Axis formatting
+# =========================
+ax_volume.set_xlabel("Mass in cup [g]")
+ax_volume.set_ylabel("Flow rate [g/s]")
+ax_volume.set_ylim(top=2.2)
+
+ax_solutes.set_xlabel("Disolved solids [g]")
+ax_solutes.set_ylabel("Flow rate [g/s]")
+ax_solutes.set_ylim(top=2.2)
+
+# =========================
+# Panel labels
+# =========================
+panel_labels = [r"\textbf{(A)}", r"\textbf{(B)}"]
+for ax, label in zip(axs, panel_labels):
+    ax.text(
+        0.05, 0.90, label,
+        transform=ax.transAxes,
+        fontsize=FONT_SIZE,
+        fontweight="bold",
+    )
+
+fig.set_constrained_layout_pads(w_pad=0.05, h_pad=0.02, wspace=0.02, hspace=0.02)
+out_file = OUTPUT_DIR / "multipanel_tds_vs_volume.pdf"
+
+plt.savefig(out_file)
+if __name__ == "__main__":
+    plt.show()
+plt.close(fig)
+
+print(f"Saved {out_file}")
+
